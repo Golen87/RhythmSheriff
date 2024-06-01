cee810afcd2d6ea8c54f21bc143623eb02ab664f
Golen <golen87@hotmail.com>
2024-06-01 05:37:57 +0200

    Convert to tabs

--- src/neu-main.ts
+++ src/neu-main.ts
@@ -1,14 +1,14 @@
-import * as Neutralino from '@neutralinojs/lib';
-type Neutralino = typeof import('@neutralinojs/lib');
+import * as Neutralino from "@neutralinojs/lib";
+type Neutralino = typeof import("@neutralinojs/lib");
 
 const NeutralinoLoad = (Neutralino: Neutralino) => {
-    Neutralino.window.center();
-    Neutralino.events.on("windowClose", () => {
-        Neutralino.app.exit();
-    });
-}
+	Neutralino.window.center();
+	Neutralino.events.on("windowClose", () => {
+		Neutralino.app.exit();
+	});
+};
 
-if( window.NL_TOKEN ) {
-    Neutralino.init();
-    NeutralinoLoad(Neutralino);
+if (window.NL_TOKEN) {
+	Neutralino.init();
+	NeutralinoLoad(Neutralino);
 }
